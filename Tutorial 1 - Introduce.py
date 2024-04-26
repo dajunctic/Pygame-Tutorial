b59825e08c04ed3f6ac29ae48ef1c70b15f87230
Dajunctic Vu <59690194+Dajunctic@users.noreply.github.com>
2024-04-26 16:00:51 +0700

    Update Lesson 1

---
+++ Tutorial 1 - Introduce.py
@@ -0,0 +1,42 @@
+import pygame
+
+RESOLUTION = (1280, 720)
+FPS = 60
+
+
+class App:
+    def __init__(self):
+        self.surface = pygame.display.set_mode(RESOLUTION)
+        pygame.display.set_caption("Pygame Tutorial")
+
+        self.clock = pygame.time.Clock()
+        self.x, self.y = 100, 100
+
+    def run(self):
+        is_running = True
+
+        while is_running:
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    is_running = False
+
+            self.update()
+            self.draw()
+
+            pygame.display.flip()
+            self.clock.tick(FPS)
+
+    def update(self):
+        if self.x + 100 < 1280 and self.y + 100 < 720:
+            self.x += 1
+            self.y += 1
+
+    def draw(self):
+        pygame.draw.rect(self.surface, (0, 0, 0), (0, 0, 1280, 720))
+
+        pygame.draw.rect(self.surface, (255, 255, 255), (self.x, self.y, 100, 100), 1)
+
+
+if __name__ == '__main__':
+    game = App()
+    game.run()
